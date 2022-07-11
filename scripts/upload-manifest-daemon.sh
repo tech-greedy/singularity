7a8a95ab9aa70e52f153a6a0305c26201130c55e
X Xu <102283833+liuziba@users.noreply.github.com>
2022-07-10 23:05:51 -0700

    Update upload-manifest-daemon.sh

--- scripts/upload-manifest-daemon.sh
+++ scripts/upload-manifest-daemon.sh
@@ -17,22 +17,24 @@ if [ -z "${WEB3_STORAGE_TOKEN}" ]; then
 fi
 if [ "$#" -lt 2 ]; then
   echo "Argument not supplied. Example: ./upload-manifest-daemon.sh <datasetName> <slugName> [data_preparation_service_endpoint]"
-  echo "By default, data_preparation_service_endpoint is set to http://127.0.0.1:7001 if you haven't modified default.toml in singularity repo"
+  echo "<datasetName> is the dataset name shown in 'singularity prep list'"
+  echo "<slugName> is the slug name displayed on Slingshot V3 website"
+  echo "[data_preparation_service_endpoint] is optional and by default set to http://127.0.0.1:7001"
   exit 1
 fi
 endpoint=${3:-http://127.0.0.1:7001}
 name=$1
 slug=$2
-for row in $(curl ${endpoint}/preparation/${name} 2>/dev/null | jq -c '.generationRequests[]|del(.errorMessage)')
+if curl ${endpoint}/preparation/${name} 2>/dev/null | jq -c '.scanningStatus, .generationRequests[].status' | grep -v completed; then
+  echo "There are still generations not completed. Please check 'singularity prep list'"
+  exit 1
+fi
+for row in $(curl ${endpoint}/preparation/${name} 2>/dev/null | jq -c '.generationRequests[]|[.id, .pieceCid, .index]')
 do
-  id=$(jq '.id' <<< "$row" | tr -d '"')
-  pieceCid=$(jq '.pieceCid' <<< "$row" | tr -d '"')
-  index=$(jq '.index' <<< "$row" | tr -d '"')
-  status=$(jq '.status' <<< "$row" | tr -d '"')
-  if [ "$status" != "completed" ]; then
-    echo "Skipped [${index}] as the status is ${status}."
-  fi
-  echo "Working on [${index}] - generation id: ${id}"
+  id=$(jq '.[0]' <<< "$row" | tr -d '"')
+  pieceCid=$(jq '.[1]' <<< "$row" | tr -d '"')
+  index=$(jq '.[2]' <<< "$row" | tr -d '"')
+  echo "Working on [${index}] - generation id: ${id}, pieceCid: ${pieceCid}"
   curl ${endpoint}/generation-manifest/${id} 2>/dev/null | jq ".dataset = \"${slug}\""| zstd -f | curl -X POST -H "Authorization: Bearer ${WEB3_STORAGE_TOKEN}" -H "X-NAME: ${pieceCid}.json.zst" --data-binary @- https://api.web3.storage/upload
   echo
 done
