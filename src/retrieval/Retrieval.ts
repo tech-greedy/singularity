d0c0f4de863a1b1a1ede9ba9ed1162f9ca2308eb
liuziba <lisashane16@gmail.com>
2022-07-14 17:08:50 -0700

    build fix

--- src/retrieval/Retrieval.ts
+++ src/retrieval/Retrieval.ts
@@ -6,7 +6,7 @@ import { spawnSync } from 'child_process';
 import fs, { ReadStream, WriteStream } from 'fs';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { rrdir } from '../deal-preparation/rrdir';
+import { rrdirSync } from '../deal-preparation/rrdir';
 
 export interface FileStat {
   type: 'file' | 'dir',
@@ -137,7 +137,7 @@ export default class Retrieval {
         process.exit(1);
       }
 
-      for (const entry of rrdir.sync(tempDir, { stats: true })) {
+      for (const entry of rrdirSync(tempDir, { stats: true })) {
         const relative = pth.relative(tempDir, entry.path);
         const target = pth.join(dest, relative);
         if (entry.directory) {
