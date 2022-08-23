9b61bf26c8536f18aaf51e010fe40333ff8898ee
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2022-08-23 12:43:55 -0700

    fix

--- tests/deal-preparation/worker/TrafficMonitor.spec.ts
+++ tests/deal-preparation/worker/TrafficMonitor.spec.ts
@@ -3,15 +3,15 @@ import { sleep } from '../../../src/common/Util';
 
 describe('TrafficMonitor', () => {
   it('should count the download speed', async () => {
-    const monitor = new TrafficMonitor(100);
+    const monitor = new TrafficMonitor(1000);
     monitor.countNewChunk(new Int8Array(888));
     expect(monitor.downloaded).toEqual(888);
     monitor.countNewChunk(new Int8Array(888 ));
     expect(monitor.downloaded).toEqual(888 * 2);
-    await sleep(20);
+    await sleep(100);
     monitor.countNewChunk(new Int8Array(888 ));
     expect(monitor.downloaded).toEqual(888 * 3);
-    await sleep(100);
+    await sleep(1000);
     monitor.countNewChunk(new Int8Array(888));
     expect(monitor.downloaded).toEqual(888);
   })
