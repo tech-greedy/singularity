3de2d40d6da97d2a340812b5e17603a2a8acc4d0
Nathanial Marsh <marshyonline@gmail.com>
2022-12-01 16:19:18 +1100

    Now with 100% coverage!

--- tests/common/GenerateCsv.spec.ts
+++ tests/common/GenerateCsv.spec.ts
@@ -10,9 +10,36 @@ describe('GenerateCsv', () => {
     await Datastore.ReplicationRequestModel.deleteMany();
     await Datastore.DealStateModel.deleteMany();
   });
+  it ('should fail export with no deals', async () => {
+    const request = await Datastore.ReplicationRequestModel.create({
+      storageProviders: 'f01001',
+      status: 'completed',
+      urlPrefix: 'http://localhost:3000'
+    });
+    const runCSV = await GenerateCsv.generate(request.id, '/tmp');
+    await expect(runCSV).toEqual(`No deal found to export in ${request.id}\n`)
+  }) 
   it ('should be able to gen with valid id', async () => {
     const request = await Datastore.ReplicationRequestModel.create({
       storageProviders: 'f01001',
+      status: 'completed',
+      urlPrefix: 'http://localhost:3000/'
+    });
+    await Datastore.DealStateModel.create({
+      dealId: 2,
+      client: 'f01001',
+      provider: 'f01001',
+      state: 'proposed',
+      replicationRequestId: request.id
+    });
+    const runCSV = await GenerateCsv.generate(request.id, '/tmp');
+    await expect(runCSV).toEqual(`CSV saved to /tmp/f01001_${request.id}.csv\n`)
+  })
+
+  it ('should be able to gen with valid id with file list path', async () => {
+    const request = await Datastore.ReplicationRequestModel.create({
+      storageProviders: 'f01001',
+      fileListPath: '-path-',
       status: 'completed',
       urlPrefix: 'http://localhost:3000'
     });
@@ -20,12 +47,15 @@ describe('GenerateCsv', () => {
       dealId: 2,
       client: 'f01001',
       provider: 'f01001',
-      state: 'active',
+      state: 'proposed',
       replicationRequestId: request.id
     });
-    await GenerateCsv.generate(request.id, '/tmp');
+    const runCSV = await GenerateCsv.generate(request.id, '/tmp');
+    await expect(runCSV).toEqual(`CSV saved to /tmp/f01001_-path-_${request.id}.csv\n`)
   })
+
   it ('should not be able to gen without valid id', async () => {
-    await GenerateCsv.generate('634b71ada25b9be2a58e434c', '/tmp');
+    const runCSV = await GenerateCsv.generate('634b71ada25b9be2a58e434c', '/tmp');
+    await expect(runCSV).toEqual(`Replication request not found 634b71ada25b9be2a58e434c`)
   })
 });
